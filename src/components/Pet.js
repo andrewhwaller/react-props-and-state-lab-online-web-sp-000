38773d546020ecdf2e17a834f5d44a950a63a7bf
Andrew Waller <andrewhwaller@gmail.com>
2019-09-27 00:20:33 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -6,7 +6,6 @@ class Pet extends React.Component {
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
             {this.props.pet.name}{' '}
             {this.props.pet.gender === 'female' ? '♀' : '♂'}
           </a>
